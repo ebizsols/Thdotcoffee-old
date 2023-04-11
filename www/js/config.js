5a7646f2c9d50e09f1cd281b29897d268aef09b9
ebizsols <director@premiumebizsols.com>
2023-04-12 04:12:02 +0900

    Project's Initial Commit

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+var krms_config ={			
+	'ApiUrl':"https://thedotcoffee.com/orders/singlemerchant/api",
+	'WPApiUrl':"https://thedotcoffee.com/wp-json/wp/v2",			
+	'DialogDefaultTitle':"The Dot Coffee & Brunch",			
+	'MerchantKeys':"306f0ec73b7e428dbb8e0174971f49e9",
+	'debug': false
+};
+
