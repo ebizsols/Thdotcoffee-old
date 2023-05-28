4cbe337ef2d2ac1253f2b3a784e177c37a8a6519
ebizsols <director@premiumebizsols.com>
2023-05-28 16:59:06 +0900

    last updates to setup project

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 var krms_config ={			
 	'ApiUrl':"https://thedotcoffee.com/orders/singlemerchant/api",
 	'WPApiUrl':"https://thedotcoffee.com/wp-json/wp/v2",			
-	'DialogDefaultTitle':"The Dot Coffee & Brunch",			
+	'DialogDefaultTitle':"The Dot Coffee Roasters",			
 	'MerchantKeys':"306f0ec73b7e428dbb8e0174971f49e9",
 	'debug': false
 };
